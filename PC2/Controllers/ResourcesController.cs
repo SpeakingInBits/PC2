db11db51925e5d84467c93c29d65513d602b71be
Thom Barrett <thom440@yahoo.com>
2021-10-01 21:52:31 -0700

    Started creating view ResourceGuide

---
+++ PC2/Controllers/ResourcesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PC2.Data;
+using PC2.Models;
+
+namespace PC2.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResourcesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ResourceGuide()
+        {
+            List<AgencyCategory> agencyCategories = await AgencyCategoryDB.GetAgencyCategoriesAsync(_context);
+            return View(agencyCategories);
+        }
+    }
+}
