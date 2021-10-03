3d10ee82b2b39a32a185d3ff02b08b78b269391c
Thom Barrett <thom440@yahoo.com>
2021-10-03 16:22:11 -0700

    Trying to set scroll position after clicking link

---
+++ PC2/wwwroot/js/resource-guide.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    window.scrollTo(0, yPos);
+}
+
+function getYpos() {
+    let y = document.documentElement.scrollTop;
+    let url = $(this).attr("href");
+    url = url.replace("tempY", y);
+    console.log(url);
+    $(this).attr("href", url);
+}
