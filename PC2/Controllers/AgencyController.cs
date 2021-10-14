f97055a32693f009553636d3b53be0d9d6a4869b
Thom Barrett <thom440@yahoo.com>
2021-10-13 20:54:38 -0700

    Created Agency Index page

---
+++ PC2/Controllers/AgencyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PC2.Data;
+using PC2.Models;
+
+namespace PC2.Controllers
+{
+    public class AgencyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AgencyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Agency> agencies = await AgencyDB.GetAllAgenciesAsync(_context);
+            return View(agencies);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
