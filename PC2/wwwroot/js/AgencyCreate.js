fec2943a20c25becd47b90f7b5d4b92c88099d23
Thom Barrett <thom440@yahoo.com>
2021-10-14 21:41:38 -0700

    Added javascript to add services to an array

---
+++ PC2/wwwroot/js/AgencyCreate.js
@@ -0,0 +1,14 @@
+﻿let services = new Array();
+
+window.onload = function () {
+    let servicesButton = document.getElementById("category-submit");
+    servicesButton.onclick = addService;
+}
+
+function addService() {
+    if ($('[name="list-of-categories"]').val().trim() != "") {
+        services.push($('[name="list-of-categories"]').val());
+        document.getElementById("category-success").innerHTML = $('[name="list-of-categories"]').val() + " added";
+        document.getElementById("list-of-service").value = "";
+    }
+}
