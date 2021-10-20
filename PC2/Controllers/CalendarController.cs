f8d2eb880d9c7fd35059d7adf3031a632278d58d
Thom Barrett <thom440@yahoo.com>
2021-10-19 21:01:48 -0700

    Created CalendarController

---
+++ PC2/Controllers/CalendarController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PC2.Data;
+
+namespace PC2.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CalendarController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Sets date to today at midnight
+            //DateTime today = DateTime.Today;
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
