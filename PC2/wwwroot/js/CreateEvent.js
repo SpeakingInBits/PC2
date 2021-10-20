fc18512251fe3c4f5f0c904d7a8211475889c06a
Thom Barrett <thom440@yahoo.com>
2021-10-19 21:02:27 -0700

    Started Create events page

---
+++ PC2/wwwroot/js/CreateEvent.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    instance = new dtsel.DTS('input[name="dateTimePicker"]', {
+        showTime: false,
+        showDate: true,
+        dateFormat: "mm/dd/yyyy",
+        direction: 'BOTTOM',
+        paddingX: 5,
+        paddingY: 5
+    });
+}
