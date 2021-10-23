dcca485a24f3b67efdcf46b817a6195d6b64aa64
Thom Barrett <thom440@yahoo.com>
2021-10-22 18:14:30 -0700

    Events are displayed on the proper page

--- PC2/Controllers/EventsController.cs
+++ PC2/Controllers/EventsController.cs
@@ -20,5 +20,51 @@ namespace PC2.Controllers
             
             return View(events);
         }
+
+        public async Task<IActionResult> EventsAndActivities()
+        {
+            EventsModel eventsModel = new EventsModel();
+            eventsModel.CalendarDate = await CalendarDateDB.GetAllDates(_context);
+            for (int i = 0; i < eventsModel.CalendarDate.Count; i++)
+            {
+                for (int j = 0; j < eventsModel.CalendarDate[i].Events.Count; j++)
+                {
+                    // If it is not a PC2 event then it should not be in the list
+                    if (eventsModel.CalendarDate[i].Events[j].CountyEvent == false)
+                    {
+                        eventsModel.CalendarDate[i].Events.RemoveAt(j);
+                    }
+                }
+                // If the events list is empty the date needs to be removed
+                if (eventsModel.CalendarDate[i].Events.Count == 0)
+                {
+                    eventsModel.CalendarDate.RemoveAt(i);
+                }
+            }
+            return View(eventsModel);
+        }
+
+        public async Task<IActionResult> CountyWideEventsAndActivities()
+        {
+            EventsModel eventsModel = new EventsModel();
+            eventsModel.CalendarDate = await CalendarDateDB.GetAllDates(_context);
+            for (int i = 0; i < eventsModel.CalendarDate.Count; i++)
+            {
+                for (int j = 0; j < eventsModel.CalendarDate[i].Events.Count; j++)
+                {
+                    // If it is not a county event then it should not be in the list
+                    if (eventsModel.CalendarDate[i].Events[j].PC2Event == false)
+                    {
+                        eventsModel.CalendarDate[i].Events.RemoveAt(j);
+                    }
+                }
+                // If the events list is empty the date needs to be removed
+                if (eventsModel.CalendarDate[i].Events.Count == 0)
+                {
+                    eventsModel.CalendarDate.RemoveAt(i);
+                }
+            }
+            return View(eventsModel);
+        }
     }
 }
