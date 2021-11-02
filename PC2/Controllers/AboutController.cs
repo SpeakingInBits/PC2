b0d6a3923298390fe0dddf7463efd7cfff7ab82d
Thom Barrett <thom440@yahoo.com>
2021-11-02 14:58:28 -0700

    Able to create a staff member

---
+++ PC2/Controllers/AboutController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PC2.Data;
+using PC2.Models;
+
+namespace PC2.Controllers
+{
+    [Authorize(Roles = IdentityHelper.Admin)]
+    public class AboutController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AboutController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Creates a staff member
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult CreateStaff()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStaff(Staff staff)
+        {
+            await StaffDB.AddStaff(_context, staff);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PC2/Data/StaffDB.cs
@@ -0,0 +1,13 @@
+﻿using PC2.Models;
+
+namespace PC2.Data
+{
+    public static class StaffDB
+    {
+        public static async Task AddStaff(ApplicationDbContext context, Staff staff)
+        {
+            context.StaffMembers.Add(staff);
+            await context.SaveChangesAsync();
+        }
+    }
+}
