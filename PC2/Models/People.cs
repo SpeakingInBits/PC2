e847dedc710d50d1a0915d2217b4d5c8b151219d
Thom Barrett <thom440@yahoo.com>
2021-11-02 14:57:35 -0700

    Created Model for people, Board Members and staff

---
+++ PC2/Models/People.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PC2.Models
+{
+    public class People
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+    }
+}
