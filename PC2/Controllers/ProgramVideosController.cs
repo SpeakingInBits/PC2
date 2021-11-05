5a5c0fd6589d573918642a2c60376d0160b5fd40
Thom Barrett <thom440@yahoo.com>
2021-11-05 12:24:32 -0700

    Created ProgramVideos page. In order to work javascript needs to be embeded.

---
+++ PC2/Controllers/ProgramVideosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PC2.Controllers
+{
+    public class ProgramVideosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
