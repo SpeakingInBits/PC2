f125edb2673a99c645acec46936822e842796c79
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-12-16 13:48:30 -0800

    Send External Links to Seperate Tab Added a JS file with small script to send external links to a new tab.

---
+++ PC2/wwwroot/js/ExternalLinkTab.js
@@ -0,0 +1,15 @@
+﻿// Opens external links in a new tab
+
+(function () {
+    const links = document.querySelectorAll("a[href^='https://'], a[href^='http://']");
+    const host = window.location.hostname
+
+    const internalLink = link => new URL(link).hostname === host
+
+    links.forEach(link => {
+        if (internalLink(link)) return
+
+        link.setAttribute("target", "blank")
+        link.setAttribute("rel", "noopener")
+    })      
+})()
