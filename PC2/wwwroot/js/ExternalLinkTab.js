8552e5c91cdceed873c8fe7ab0550b373614a250
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-12-16 14:14:13 -0800

    added a check incase othe links have "target" "blank" all ready

--- PC2/wwwroot/js/ExternalLinkTab.js
+++ PC2/wwwroot/js/ExternalLinkTab.js
@@ -2,14 +2,18 @@
 
 (function () {
     const links = document.querySelectorAll("a[href^='https://'], a[href^='http://']");
-    const host = window.location.hostname
+    const host = window.location.hostname;
 
     const internalLink = link => new URL(link).hostname === host
 
     links.forEach(link => {
-        if (internalLink(link)) return
-
-        link.setAttribute("target", "blank")
-        link.setAttribute("rel", "noopener")
-    })      
+        if (internalLink(link)) return;
+        if (link.getAttribute("target", "blank")) {
+            return link;
+        }
+        else {
+            link.setAttribute("target", "blank");
+            link.setAttribute("rel", "noopener");
+        }
+    });
 })()
