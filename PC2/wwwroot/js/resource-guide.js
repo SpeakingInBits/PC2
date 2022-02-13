4d77337e589b3fad0f755c3583958c75a3bb5413
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2022-02-13 11:39:21 -0800

    Changed JS to route city correctly when searching by city

--- PC2/wwwroot/js/resource-guide.js
+++ PC2/wwwroot/js/resource-guide.js
@@ -3,10 +3,10 @@
     let agency = document.getElementsByClassName("right-agency");
     let agency2 = document.getElementsByClassName("left-agency");
     let categorySubmit = document.getElementById("category-submit");
-    let zipSubmit = document.getElementById("zip-submit");
+    let citySubmit = document.getElementById("city-submit");
     agencySubmit.onclick = setAgencyNameAndGetYPos;
     categorySubmit.onclick = setCategoryNameAndGetYPos;
-    zipSubmit.onclick = setZipcodeAndGetYPos;
+    citySubmit.onclick = setCityAndGetYPos;
 
     for (let i = 0; i < agency.length; i++) {
         agency[i].addEventListener('click', getYpos);
@@ -45,12 +45,12 @@ function setCategoryNameAndGetYPos() {
     $(this).attr("formaction", url);
 }
 
-function setZipcodeAndGetYPos() {
+function setCityAndGetYPos() {
     let y = document.documentElement.scrollTop;
     let url = $(this).attr("formaction");
     url = url.replace("tempY", y);
 
-    let input = $('[name="list-of-zipcodes"]').val();
-    url = url.replace("zip-code", input);
+    let input = $('[name="list-of-cities"]').val();
+    url = url.replace("city-name", input);
     $(this).attr("formaction", url);
 }
