a3a7739d4621837d91c20b64c36ca9ba916d332d
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-06-03 11:03:29 -0700

    Issue #111 housing program table (#126)
    * Add HousingProgram model and DB migration
    
    * Made housing program page pull from DB
    
    * Add housing program data to DB Context
    
    * Add admin page to update housing program data

---
+++ PC2/Data/Migrations/20220603045021_AddHousingProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PC2.Models;
+
+#nullable disable
+
+namespace PC2.Data.Migrations
+{
+    public partial class AddHousingProgram : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HousingProgram",
+                columns: table => new
+                {
+                    HouseHoldSize = table.Column<int>(type: "int", nullable: false),
+                    MaximumIncome = table.Column<double>(type: "float", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HousingProgram", x => x.HouseHoldSize);
+                });
+
+            // Insert initial table data
+            double[] maxIncomes = new double[] { 50900, 58150, 65400, 72650, 78500, 84300, 90100, 95900 };
+            for (int i = 0; i < maxIncomes.Length; i++)
+            {
+                AddHousingProgramRow(migrationBuilder, i + 1, maxIncomes[i]);
+            }
+
+            static void AddHousingProgramRow(MigrationBuilder migrationBuilder, int houseHoldSize, double maxIncome)
+            {
+                migrationBuilder.InsertData(table: "HousingProgram",
+                                columns: new[] { nameof(HousingProgram.HouseHoldSize), nameof(HousingProgram.MaximumIncome), nameof(HousingProgram.LastUpdated) },
+                                values: new object[] { houseHoldSize, maxIncome, "9/1/2021" });
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HousingProgram");
+        }
+    }
+}
---
+++ PC2/Models/HousingProgram.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PC2.Models
+{
+    public class HousingProgram
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Household Size")]
+        public int HouseHoldSize { get; set; }
+
+        [Display(Name="Maximum Income")]
+        [DataType(DataType.Currency)]
+        public double MaximumIncome { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastUpdated { get; set; }
+    }
+}
