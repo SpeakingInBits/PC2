30ab6df463b3fd999d01b7c2c4361e34cabc578f
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-07-12 12:41:06 -0700

    Issue #153 track page views (#159)
    * Add Result filter for ApplicationInsights PageView tracking
    
    * Register ResultFilter

---
+++ PC2/Filters/ApplicationInsightsPageViewTracker.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace PC2.Filters
+{
+    public class ApplicationInsightsPageViewTracker : IAsyncResultFilter
+    {
+        private readonly TelemetryClient _telemetryClient;
+
+        public ApplicationInsightsPageViewTracker(TelemetryClient telemetryClient)
+        {
+            this._telemetryClient = telemetryClient;
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            // Get current URL
+            string url = context.HttpContext.Request.Path;
+            _telemetryClient.TrackPageView(url);
+            await next();
+        }
+    }
+}
