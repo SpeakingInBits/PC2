a1eb7f3090e6addc5e5abd22dae583782b248c21
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-09-20 22:40:14 -0700

    Remove CalendarDate class

--- PC2/Models/CalendarEvent.cs
+++ PC2/Models/CalendarEvent.cs
@@ -2,24 +2,47 @@
 
 namespace PC2.Models
 {
-    public class CalendarEvent
+    public class CalendarEvent : IComparable<CalendarEvent>
     {
         [Key]
         public int CalendarEventID { get; set; }
 
+        /// <summary>
+        /// Day the event is taking place
+        /// </summary>
+        public DateOnly DateOfEvent { get; set; }
+
+        /// <summary>
+        /// Start time of the event
+        /// </summary>
         [Required]
         public TimeOnly StartingTime {  get; set; }
 
+        /// <summary>
+        /// End time of the event
+        /// </summary>
         [Required]
         public TimeOnly EndingTime {  get; set; }
 
+        /// <summary>
+        /// Description of the Event
+        /// </summary>
         [Required]
-        public string EventDescription {  get; set; }
+        public string EventDescription { get; set; } = null!;
 
+        /// <summary>
+        /// True if the event is a PC2 event
+        /// </summary>
         public bool PC2Event {  get; set; }
 
+        /// <summary>
+        /// True if the event is a county event
+        /// </summary>
         public bool CountyEvent {  get; set; }
 
-        public CalendarDate CalendarDate {  get; set; }
+        public int CompareTo(CalendarEvent? other)
+        {
+            return this.DateOfEvent.CompareTo(other.DateOfEvent);
+        }
     }
 }
--- PC2/Models/EventsModel.cs
+++ PC2/Models/EventsModel.cs
@@ -2,7 +2,7 @@
 {
     public class EventsModel
     {
-        public List<CalendarDate> CalendarDate {  get; set; }
+        public List<CalendarEvent> CalendarEvents {  get; set; }
 
         public bool IsPC2EventCalendar { get; set; } = true;
     }
