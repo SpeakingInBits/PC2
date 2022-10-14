6107d537eeeea0ff689f27fefcb6a34f5d7cb7eb
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-10-14 15:34:44 -0700

    Change newsletter name and location to required strings

--- PC2/Models/NewsletterFile.cs
+++ PC2/Models/NewsletterFile.cs
@@ -8,10 +8,10 @@ namespace PC2.Models
         public int NewsletterId { get; set; }
 
         [Required] // must be the filename
-        public string? Name { get; set; }
+        public string Name { get; set; } = null!;
 
         [Required] // used to create dynamic links
-        public string? Location { get; set; }
+        public string Location { get; set; } = null!;
 
     }
 }
