6c0b6a06e5bdc20d190a2a7f29d5698bb30fe40f
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-11-23 12:30:13 -0800

    JS targeted "blank" instead of "_blank"

--- PC2/wwwroot/js/site.js
+++ PC2/wwwroot/js/site.js
@@ -3,16 +3,16 @@
 (function () {
     const links = document.querySelectorAll("a[href^='https://'], a[href^='http://']");
     const host = window.location.hostname;
-
+    
     const internalLink = link => new URL(link).hostname === host
 
     links.forEach(link => {
         if (internalLink(link)) return;
-        if (link.getAttribute("target", "blank")) {
+        if (link.getAttribute("target", "_blank")) {
             return link;
         }
         else {
-            link.setAttribute("target", "blank");
+            link.setAttribute("target", "_blank");
             link.setAttribute("rel", "noopener");
         }
     });
