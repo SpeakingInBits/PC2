512c65aa1783890a64abbdd027fc0fa9fe7821a9
Campbell Rex <36382100+crex424@users.noreply.github.com>
2023-07-24 19:41:25 -0700

    added documentation for ContactPageModel and all fields (#249)
    * added documentation for ContactPageModel and all fields
    
    * Refactored documentation for ContactPageModel to be more accurate to the the class's purpose
    
    * Refactored documentation for field Message to be less vague.

--- PC2/Models/ContactPageModel.cs
+++ PC2/Models/ContactPageModel.cs
@@ -1,11 +1,29 @@
 ﻿namespace PC2.Models
 {
+    /// <summary>
+    /// Represents the contact request from a website visitor
+    /// </summary>
     public class ContactPageModel
     {
+        /// <summary>
+        /// The contact's name
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The contact's email address
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// The contact's phone number
+        /// </summary>
         public string Phone { get; set; }
+        /// <summary>
+        /// The subject of the message
+        /// </summary>
         public string Subject { get; set; }
+        /// <summary>
+        /// The message written for the contact by a website visitor
+        /// </summary>
         public string Message { get; set; }
     }
 }
