26fce567036c55334089ba472937dc7759362d25
Campbell Rex <campbellrex@outlook.com>
2023-07-31 17:21:26 -0700

    First attempt at documenting the NewsLetterFile

--- PC2/Models/NewsletterFile.cs
+++ PC2/Models/NewsletterFile.cs
@@ -2,14 +2,26 @@
 
 namespace PC2.Models
 {
+    /// <summary>
+    /// Represents the NewsLetterFile
+    /// </summary>
     public class NewsletterFile
     {
+        /// <summary>
+        /// The unique identifier for the NewsLetter
+        /// </summary>
         [Key] // for naming the newsletter  
         public int NewsletterId { get; set; }
 
+        /// <summary>
+        /// The name of the NewsLetterFile
+        /// </summary>
         [Required] // must be the filename
         public string Name { get; set; } = null!;
 
+        /// <summary>
+        /// The location of the NewsLetterFile in the disk
+        /// </summary>
         [Required] // used to create dynamic links
         public string Location { get; set; } = null!;
 
