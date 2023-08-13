9cb723580710babe9c1168dc1f2da9c89236cf1f
Campbell Rex <campbellrex@outlook.com>
2023-08-13 15:04:58 -0700

    Documented AgencyCategory and all contained fields

--- PC2/Models/AgencyCategory.cs
+++ PC2/Models/AgencyCategory.cs
@@ -2,13 +2,25 @@
 
 namespace PC2.Models
 {
+    /// <summary>
+    /// Represents a agency category which may contain many agencies
+    /// </summary>
     public class AgencyCategory
     {
+        /// <summary>
+        /// The unique identifier for AgencyCategory
+        /// </summary>
         [Key]
         public int AgencyCategoryId {  get; set; }
-        
+
+        /// <summary>
+        /// The agency category's name
+        /// </summary>
         public string AgencyCategoryName {  get; set; }
 
+        /// <summary>
+        /// A list of agencies
+        /// </summary>
         public List<Agency> Agencies {  get; set; } = new List<Agency>();
     }
 }
