7dd86fe4de0cdc555cb4655ca09c9298782510bc
Trang Truong <118577692+trangtruongus@users.noreply.github.com>
2023-12-07 15:16:41 -0600

    Documented Staff model and moved into People.cs (#279)

--- PC2/Models/People.cs
+++ PC2/Models/People.cs
@@ -2,6 +2,9 @@
 
 namespace PC2.Models
 {
+    /// <summary>
+    /// Represents a person with basic information.
+    /// </summary>
     public class People
     {
         [Key]
@@ -22,5 +25,48 @@ namespace PC2.Models
         /// is a higher priority
         /// </summary>
         public byte PriorityOrder { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a staff member, inheriting basic information from <see cref="People"/>.
+    /// </summary>
+    public class Staff : People
+    {
+        /// <summary>
+        /// The staff/person's phone number.
+        /// </summary>
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// The phone extension.
+        /// </summary>
+        public int? Extension { get; set; }
+
+        /// <summary>
+        /// The staff/person's email address.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets a formatted display of the phone number and extension.
+        /// </summary>
+        public string? PhoneDisplay
+        {
+            get
+            {
+                if (Phone != null && Extension != null)
+                {
+                    return $"{Phone} ext. {Extension}";
+                }
+                else if (Phone != null)
+                {
+                    return Phone;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
