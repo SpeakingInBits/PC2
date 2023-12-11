96fac87e741b904a295c003d2168f30283df4876
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-12-11 10:00:10 -0800

    Change 2FA name in the Authenticator app

--- PC2/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ PC2/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -180,7 +180,7 @@ namespace PC2.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("PC2Online.org"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
