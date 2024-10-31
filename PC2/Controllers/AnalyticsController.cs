cc632c46d3d2d09242adc0bb9d4754f76e29acb6
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2024-10-30 21:20:42 -0700

    Update AnalyticsController to fetch historical data from Azure Application Insights
    * **AnalyticsController.cs**
      - Add `GetHistoricalData` method to fetch historical data from Azure Application Insights
      - Update `Index` method to call `GetHistoricalData` and pass data to the view
      - Refactor to use `APPLICATIONINSIGHTS_CONNECTION_STRING`
    
    * **Index.cshtml**
      - Update JavaScript code to handle new data format returned by `GetHistoricalData`
      - Update chart initialization code to use historical data

--- PC2/Controllers/AnalyticsController.cs
+++ PC2/Controllers/AnalyticsController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.ApplicationInsights.DataContracts;
 using System.Threading.Tasks;
 using PC2.Data;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace PC2.Controllers
 {
@@ -11,18 +15,20 @@ namespace PC2.Controllers
     public class AnalyticsController : Controller
     {
         private readonly TelemetryClient _telemetryClient;
+        private readonly IConfiguration _configuration;
 
-        public AnalyticsController(TelemetryClient telemetryClient)
+        public AnalyticsController(TelemetryClient telemetryClient, IConfiguration configuration)
         {
             _telemetryClient = telemetryClient;
+            _configuration = configuration;
         }
 
         public async Task<IActionResult> Index()
         {
-            var pageViews = await GetMetricData("Page views");
-            var uniqueVisitors = await GetMetricData("Unique visitors");
-            var sessionDuration = await GetMetricData("Session duration");
-            var bounceRate = await GetMetricData("Bounce rate");
+            var pageViews = await GetHistoricalData("PageViews");
+            var uniqueVisitors = await GetHistoricalData("UniqueVisitors");
+            var sessionDuration = await GetHistoricalData("SessionDuration");
+            var bounceRate = await GetHistoricalData("BounceRate");
 
             ViewData["PageViews"] = pageViews;
             ViewData["UniqueVisitors"] = uniqueVisitors;
@@ -37,6 +43,30 @@ namespace PC2.Controllers
             var request = new MetricTelemetry(metricName, 1);
             _telemetryClient.TrackMetric(request);
             return await Task.FromResult(request.Sum);
+        }
+
+        private async Task<JObject> GetHistoricalData(string metricName)
+        {
+            string connectionString = _configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+            string requestUri = $"https://api.applicationinsights.io/v1/apps/{_configuration["ApplicationInsightsAppId"]}/metrics/{metricName}";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("x-api-key", connectionString);
+
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    return JObject.Parse(data);
+                }
+                else
+                {
+                    return null;
+                }
+            }
         }
     }
 }
