46ae4d29fe110a920ef67c33e0426b2cc13d1d41
Cameron White <cameronwhite4121@gmail.com>
2024-11-18 19:10:06 -0800

    Added action that sends json from the db to calendar javascript file

--- PC2/Controllers/EventsController.cs
+++ PC2/Controllers/EventsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using PC2.Data;
 using PC2.Models;
 
@@ -23,6 +25,35 @@ namespace PC2.Controllers
             };
 
             return View(eventsModel);
+        }
+
+        // Returns events in JSON format for FullCalendar
+        public async Task<IActionResult> GetEvents(string? eventType)
+        {
+            bool getPc2Events = eventType == null;
+
+            // Get events from the database
+            List<CalendarEvent> events = await CalendarEventDB.GetAllEvents(_context, getPc2Events);
+
+            // Holds simplified calendar events for FullCalendar
+            List<CalendarEvent> fullCalendarEvents = new List<CalendarEvent>();
+
+            // Loop through the events and add them to the list
+            foreach (CalendarEvent e in events)
+            {
+                var calendarEvent = new CalendarEvent
+                {
+                    EventDescription = e.EventDescription,
+                    DateOfEvent = e.DateOfEvent,
+                    StartingTime = e.StartingTime,
+                    EndingTime = e.EndingTime
+                };
+
+                fullCalendarEvents.Add(calendarEvent);
+            }
+
+            // Return events as JSON
+            return Json(fullCalendarEvents);
         }
     }
 }
