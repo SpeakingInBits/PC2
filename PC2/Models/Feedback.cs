51037a9acecb784c53696d1852e5e9182f3131a5
Scott <greatvstech@gmail.com>
2025-01-29 17:04:36 -0800

    Add Feedback class to PC2.Models with properties
    A new Feedback class has been added to the PC2.Models namespace.
    The class includes properties: Id, FoundResource, Comments, and
    SubmittedAt. The Comments property uses the MaxLength attribute
    from System.ComponentModel.DataAnnotations to limit its length
    to 500 characters.

---
+++ PC2/Models/Feedback.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PC2.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; } // Primary key
+
+        public bool FoundResource { get; set; } // True for "Yes", False for "No"
+
+        [MaxLength(500)]
+        public string? Comments { get; set; } // Optional comments from the user
+
+        public DateTime SubmittedAt { get; set; } // Timestamp when feedback is submitted
+    }
+}
