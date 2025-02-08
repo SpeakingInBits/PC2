fe94dcd89ab6cc77f8a183ac1fc66e56f13db426
bluestar7589 <bluestar7589@yahoo.com>
2025-02-08 10:57:11 -0800

    Add StandingMeeting column and update Discriminator length to create new meeting page.

---
+++ PC2/Data/Migrations/20250131032317_AddNewStandingMeetingColumn.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PC2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewStandingMeetingColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "People",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "StandingMeeting",
+                table: "CalendarEvents",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StandingMeeting",
+                table: "CalendarEvents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
