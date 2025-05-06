406c4f9a549a99dd00048df09e326b480b69cdd4
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-05-06 16:07:28 -0700

    Revert "Auto-generated files after migrations"
    This reverts commit 17fdb94e9e9b1dc0c3093d7dd2ad1d102fd9428a.

--- PC2/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ PC2/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace PC2.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.3")
+                .HasAnnotation("ProductVersion", "9.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
