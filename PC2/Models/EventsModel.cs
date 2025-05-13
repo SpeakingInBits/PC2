a42b97a672f9dacb6bb7f2730e7f153b8457502c
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-05-12 22:39:45 -0700

    Refactor event handling in EventsController and views (#377)
    Removed event type distinction from EventsController and EventsModel. Updated views to reflect changes, including static titles and improved layout. Added CSS for visual differentiation of events and modified JavaScript for event color coding.

--- PC2/Models/EventsModel.cs
+++ PC2/Models/EventsModel.cs
@@ -3,7 +3,5 @@
     public class EventsModel
     {
         public List<CalendarEvent> CalendarEvents {  get; set; } = new List<CalendarEvent>();
-
-        public bool IsPC2EventCalendar { get; set; } = true;
     }
 }
