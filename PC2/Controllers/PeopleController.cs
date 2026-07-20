ee4b8629d2c44c8ce5f5fb6f5080c184ca0d700c
JB-coding02! <justinburns2008@gmail.com>
2026-07-20 14:03:51 -0700

    Reworked and fully implemented changes to landing page to allow for Admins and staff to access a separatelanding page not as organized as I would like it currently will keep working.

--- PC2/Controllers/PeopleController.cs
+++ PC2/Controllers/PeopleController.cs
@@ -23,17 +23,33 @@ public class PeopleController : Controller
         _logger = logger;
     }
 
-    public async Task<IActionResult> Index(PersonType type)
+    public async Task<IActionResult> Index(PersonType? type = null, string? searchTerm = null)
     {
-        ViewData["PersonType"] = type;
-        IEnumerable<PersonViewModel> people = type switch
+        // Get all people from all types
+        var staff = (await StaffDB.GetAllStaffForEditing(_context)).Select(PersonViewModel.FromStaff).ToList();
+        var board = (await BoardDB.GetAllBoardMembersForEditing(_context)).Select(PersonViewModel.FromBoard).ToList();
+        var steeringCommittee = (await SteeringCommitteeDB.GetAllSteeringCommittee(_context)).Select(PersonViewModel.FromSteeringCommittee).ToList();
+
+        // Combine all people
+        var allPeople = staff.Concat(board).Concat(steeringCommittee).ToList();
+
+        // Filter by type if specified
+        if (type.HasValue)
         {
-            PersonType.Staff => (await StaffDB.GetAllStaffForEditing(_context)).Select(PersonViewModel.FromStaff),
-            PersonType.Board => (await BoardDB.GetAllBoardMembersForEditing(_context)).Select(PersonViewModel.FromBoard),
-            PersonType.SteeringCommittee => (await SteeringCommitteeDB.GetAllSteeringCommittee(_context)).Select(PersonViewModel.FromSteeringCommittee),
-            _ => []
-        };
-        return View(people);
+            allPeople = allPeople.Where(p => p.Type == type.Value).ToList();
+        }
+
+        // Filter by search term if specified
+        if (!string.IsNullOrWhiteSpace(searchTerm))
+        {
+            allPeople = allPeople.Where(p => p.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        // Store filter values in ViewData for the view
+        ViewData["SelectedType"] = type;
+        ViewData["SearchTerm"] = searchTerm;
+
+        return View(allPeople);
     }
 
     [HttpGet]
