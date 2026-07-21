f116aaad0842db6cdf03169f4f57e267ffc44e63
JB-coding02 <justinburns2008@gmail.com>
2026-07-21 11:56:56 -0700

    Updated UI in Manage Users page and made accessing main landing page possible for admins and staff as well as moved the manage Users button from the navbar into the Members dropdown.

--- PC2/Controllers/HomeController.cs
+++ PC2/Controllers/HomeController.cs
@@ -25,11 +25,6 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        // Redirect admin/staff users to admin dashboard
-        if (User.IsInRole(IdentityHelper.Admin) || User.IsInRole(IdentityHelper.Staff))
-        {
-            return RedirectToAction("Dashboard");
-        }
         return View();
     }
 
