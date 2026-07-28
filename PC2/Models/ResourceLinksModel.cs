fe061f265de987143eec1d874518f516d5a48476
JB-coding02 <justinburns2008@gmail.com>
2026-07-28 11:44:15 -0700

    Added a new class in the models folder named ResourceLinksModel.cs this class is for storing and handling Resource URLs.

---
+++ PC2/Models/ResourceLinksModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PC2.Models;
+
+public class ResourceLinksModel
+{
+    // Unique Identifier for each link.
+    public int ResourceID { get; set; }
+
+    // Unique link stored as a string.
+    public string LinkURL { get; set; } = string.Empty;
+
+    // A string containing the text that replaces the link text in the view.
+    public string LinkText { get; set; } = string.Empty;
+
+    // The first letter of the LinkText for ordering alphabetically.
+    public char FirstLetter { get; set; }
+}
